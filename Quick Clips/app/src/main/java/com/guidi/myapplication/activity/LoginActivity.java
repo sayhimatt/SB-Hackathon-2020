885fe0242bd9d61600a47cd4c674679123b5a9c5
Camposm <camposm1205@gmail.com>
2020-09-25 19:13:51 -0400

    Update LoginActivity.java

--- Quick Clips/app/src/main/java/com/guidi/myapplication/activity/LoginActivity.java
+++ Quick Clips/app/src/main/java/com/guidi/myapplication/activity/LoginActivity.java
@@ -2,7 +2,9 @@ package com.guidi.myapplication.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.guidi.myapplication.R;
 
@@ -12,5 +14,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void signup(View v) {
+        Intent intent = new Intent();
     }
 }
