2b7b7a34d574a4dc0b40dc9be8ebc087fbe603ba
Adriana S <saloa4500@mail.sunysuffolk.edu>
2020-09-26 01:49:01 -0400

    SignUpActivity.java

---
+++ QuickClips/app/src/main/java/com/fourstooges/quickclips/activity/SignUpActivity.java
@@ -0,0 +1,15 @@
+package com.fourstooges.quickclips.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.guidi.myapplication.R;
+
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
