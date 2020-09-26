937179f3c18a8f10d73a71db05af87f3fa62f717
Camposm <camposm1205@gmail.com>
2020-09-26 14:31:02 -0400

    Update CommFragment.java

--- QuickClips/app/src/main/java/com/fourstooges/quickclips/ui/comm_clips/CommFragment.java
+++ QuickClips/app/src/main/java/com/fourstooges/quickclips/ui/comm_clips/CommFragment.java
@@ -4,35 +4,39 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.fourstooges.quickclips.R;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 
 public class CommFragment extends Fragment {
     private View root;
+    private EditText tfKey;
     private ImageButton btSearch;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         root = inflater.inflate(R.layout.fragment_comm, container, false);
-        loadBtSearch();
+        load();
         return root;
     }
 
-    public void loadBtSearch() {
+    public void load() {
+        tfKey = root.findViewById(R.id.comm_search_et);
         btSearch = root.findViewById(R.id.comm_search_ib);
         btSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 System.out.println("Searching...");
-                DatabaseReference ref = FirebaseDatabase.getInstance().getReference("quickclips");
-                System.out.println(ref.getKey());
+                String key = tfKey.getText().toString();
+                Query query = FirebaseDatabase.getInstance().getReference().child("quickclips")
+                        .orderByChild("title").equalTo(key);
             }
         });
     }
