fb341202b39aed16a3690564c31e57156dc02312
Matt <mattsayshi@hotmail.com>
2020-09-26 13:20:04 -0400

    Previews current account data on the AccountFragment

---
+++ QuickClips/app/src/main/java/com/fourstooges/quickclips/ui/account/AccountViewModel.java
@@ -0,0 +1,24 @@
+package com.fourstooges.quickclips.ui.account;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.fourstooges.quickclips.database.ClipItems;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class AccountViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public AccountViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Current user: \n" + FirebaseAuth.getInstance().getCurrentUser().getEmail()
+                        + "\nAmount of Clips: "+ ClipItems.ITEMS.size());
+
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
