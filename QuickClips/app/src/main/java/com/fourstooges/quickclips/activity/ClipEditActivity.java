70252125b9fff72389ddbc055c5372fcf8f224cc
kiefuh <61067888+kiefuh@users.noreply.github.com>
2020-09-26 11:46:45 -0400

    changed layout for edit

--- QuickClips/app/src/main/java/com/fourstooges/quickclips/activity/ClipEditActivity.java
+++ QuickClips/app/src/main/java/com/fourstooges/quickclips/activity/ClipEditActivity.java
@@ -3,6 +3,9 @@ package com.fourstooges.quickclips.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.fourstooges.quickclips.R;
 
@@ -14,5 +17,16 @@ public class ClipEditActivity extends AppCompatActivity {
         setContentView(R.layout.activity_clip_edit);
         String title = getIntent().getStringExtra("key_title");
         System.out.println(title);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.clip_edit_menu, menu);
+        return true;
+    }
+
+    public void itemClicked(MenuItem item){
+       
     }
 }
