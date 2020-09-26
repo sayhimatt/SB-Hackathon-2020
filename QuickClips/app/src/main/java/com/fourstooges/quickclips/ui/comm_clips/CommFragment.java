f262eacc9369782e1328899af8ef3425b7e8c66e
Camposm <camposm1205@gmail.com>
2020-09-26 14:22:44 -0400

    Update CommFragment.java

--- QuickClips/app/src/main/java/com/fourstooges/quickclips/ui/comm_clips/CommFragment.java
+++ QuickClips/app/src/main/java/com/fourstooges/quickclips/ui/comm_clips/CommFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,11 +18,23 @@ import com.fourstooges.quickclips.R;
 
 public class CommFragment extends Fragment {
     private View root;
+    private Button btSearch;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         root = inflater.inflate(R.layout.fragment_comm, container, false);
-
+        loadBtSearch();
         return root;
+    }
+
+    public void loadBtSearch() {
+        btSearch = root.findViewById(R.id.comm_search_ib);
+        btSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Searching...");
+
+            }
+        });
     }
 }
