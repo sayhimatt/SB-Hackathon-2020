26a2d6616b122e72dbae3a87d4305f6f19bd8b3a
Camposm <camposm1205@gmail.com>
2020-09-26 03:52:23 -0400

    Update SignUpActivity.java

--- QuickClips/app/src/main/java/com/fourstooges/quickclips/activity/SignUpActivity.java
+++ QuickClips/app/src/main/java/com/fourstooges/quickclips/activity/SignUpActivity.java
@@ -2,6 +2,7 @@ package com.fourstooges.quickclips.activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -20,6 +21,9 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import static com.fourstooges.quickclips.R.drawable.rectangle_field_green;
+import static com.fourstooges.quickclips.R.drawable.rectangle_field_red;
+
 
 public class SignUpActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
@@ -35,7 +39,26 @@ public class SignUpActivity extends AppCompatActivity {
         tfPassword = findViewById(R.id.edit_text_password);
         tfConfirmPassword = findViewById(R.id.edit_confirm_text_password);
         btSignUp = findViewById(R.id.bt_sign_up);
+        tfPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.toString().isEmpty()) {
+                    tfConfirmPassword.setEnabled(false);
+                } else {
+                    tfConfirmPassword.setEnabled(true);
+                }
+            }
+        });
         tfConfirmPassword.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -51,16 +74,20 @@ public class SignUpActivity extends AppCompatActivity {
             public void afterTextChanged(Editable s) {
                 if (TextUtils.equals(s, tfPassword.getText())) {
                     btSignUp.setEnabled(true);
+                    tfPassword.setBackground(ContextCompat.getDrawable(SignUpActivity.this, rectangle_field_green));
+                    tfConfirmPassword.setBackground(ContextCompat.getDrawable(SignUpActivity.this, rectangle_field_green));
                 } else {
                     btSignUp.setEnabled(false);
+                    tfPassword.setBackground(ContextCompat.getDrawable(SignUpActivity.this, rectangle_field_red));
+                    tfConfirmPassword.setBackground(ContextCompat.getDrawable(SignUpActivity.this, rectangle_field_red));
                 }
             }
         });
     }
 
     public void signUp(View v) {
-        String email = ((EditText) findViewById(R.id.tf_email)).getText().toString();
-        String password = ((EditText) findViewById(R.id.tf_password)).getText().toString();
+        String email = tfEmail.getText().toString();
+        String password = tfPassword.getText().toString();
         if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
             Toast.makeText(this, "Please Fill In The Empty Fields", Toast.LENGTH_LONG).show();
         } else {
