b8dfc8437ed162968ad0a006c8950002605dbf6a
Camposm <camposm1205@gmail.com>
2020-09-26 11:29:06 -0400

    Update ClipRecyclerViewAdapter.java

--- QuickClips/app/src/main/java/com/fourstooges/quickclips/ui/home/ClipRecyclerViewAdapter.java
+++ QuickClips/app/src/main/java/com/fourstooges/quickclips/ui/home/ClipRecyclerViewAdapter.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,32 +25,33 @@ import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.List;
 
-public class ClipRecyclerViewAdapter extends RecyclerView.Adapter<ClipRecyclerViewAdapter.ViewHolder>{
+public class ClipRecyclerViewAdapter extends RecyclerView.Adapter<ClipRecyclerViewAdapter.ViewHolder> {
     private List<ClipItems.ClipItem> mValues;
     private HomeFragment homeFragment;
     private Context c;
 //    private FirebaseAuth mAuth;
 //    private String currentUserID;
 
-    public ClipRecyclerViewAdapter(List<ClipItems.ClipItem> items, HomeFragment homeFragment, Context c){
+    public ClipRecyclerViewAdapter(List<ClipItems.ClipItem> items, HomeFragment homeFragment, Context c) {
         mValues = items;
         this.homeFragment = homeFragment;
         this.c = c;
     }
+
     @Override
-    public ViewHolder onCreateViewHolder( ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.clip_item,parent, false);
+                .inflate(R.layout.clip_item, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(final ViewHolder holder, int position) {
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
         holder.title_tv.setText(mValues.get(position).getTitle());
         holder.main_tv.setText(mValues.get(position).getText());
         System.out.println("id=" + mValues.get(position).getId());
         // Apply Animations
-        holder.modify_b.setAnimation(AnimationUtils.loadAnimation(c, R.anim.anim_fade_trans_1));
+//        holder.modify_b.setAnimation(AnimationUtils.loadAnimation(c, R.anim.anim_fade_trans_1));
         holder.copy_b.setAnimation(AnimationUtils.loadAnimation(c, R.anim.anim_fade_trans_1));
         holder.title_tv.setAnimation(AnimationUtils.loadAnimation(c, R.anim.anim_fade_trans_1));
         holder.main_tv.setAnimation(AnimationUtils.loadAnimation(c, R.anim.anim_fade_trans_1));
@@ -58,7 +60,7 @@ public class ClipRecyclerViewAdapter extends RecyclerView.Adapter<ClipRecyclerVi
             @Override
             public boolean onLongClick(View view) {
                 Toast t = Toast.makeText(view.getContext(), "Copied!", Toast.LENGTH_SHORT);
-                t.setGravity(Gravity.CENTER,0,0);
+                t.setGravity(Gravity.CENTER, 0, 0);
                 t.show();
                 String text = holder.main_tv.getText().toString();
                 ClipboardManager clipboard = (ClipboardManager) c.getSystemService(Context.CLIPBOARD_SERVICE);
@@ -71,9 +73,11 @@ public class ClipRecyclerViewAdapter extends RecyclerView.Adapter<ClipRecyclerVi
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ClipItems.ClipItem item = mValues.get(position);
                 Intent i = new Intent(c, ClipEditActivity.class);
-                i.putExtra("key_title", holder.title_tv.getText());
-                i.putExtra("key_text", holder.main_tv.getText());
+                i.putExtra("clip_item", item);
+//                i.putExtra("key_title", holder.title_tv.getText());
+//                i.putExtra("key_text", holder.main_tv.getText());
 //                i.putExtra("key_public", holder.) END GAME STUFF
                 c.startActivity(i);
             }
@@ -83,7 +87,7 @@ public class ClipRecyclerViewAdapter extends RecyclerView.Adapter<ClipRecyclerVi
             @Override
             public void onClick(View view) {
                 Toast t = Toast.makeText(view.getContext(), "Copied!", Toast.LENGTH_SHORT);
-                t.setGravity(Gravity.CENTER,0,0);
+                t.setGravity(Gravity.CENTER, 0, 0);
                 t.show();
                 String text = holder.main_tv.getText().toString();
                 ClipboardManager clipboard = (ClipboardManager) c.getSystemService(Context.CLIPBOARD_SERVICE);
@@ -92,18 +96,16 @@ public class ClipRecyclerViewAdapter extends RecyclerView.Adapter<ClipRecyclerVi
             }
         });
 
-        holder.modify_b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-            }
-        });
-
+//        holder.modify_b.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
 
 
     }
-
-
 
 
     @Override
@@ -112,20 +114,19 @@ public class ClipRecyclerViewAdapter extends RecyclerView.Adapter<ClipRecyclerVi
     }
 
 
-
     public class ViewHolder extends RecyclerView.ViewHolder {
-            public ClipItems.ClipItem cItem;
-            public Button modify_b;
-            public Button copy_b;
-            public TextView title_tv;
-            public TextView main_tv;
+//        public ClipItems.ClipItem cItem;
+//        public Button modify_b;
+        public ImageButton copy_b;
+        public TextView title_tv;
+        public TextView main_tv;
 
         public ViewHolder(View view) {
             super(view);
-            modify_b= view.findViewById(R.id.modify_b);
-            copy_b= view.findViewById(R.id.copy_b);
-            title_tv=view.findViewById(R.id.title_tv);
-            main_tv=view.findViewById(R.id.mText_tv);
+//            modify_b= view.findViewById(R.id.modify_b);
+            copy_b = view.findViewById(R.id.bt_copy);
+            title_tv = view.findViewById(R.id.title_tv);
+            main_tv = view.findViewById(R.id.mText_tv);
         }
 
         /**
@@ -134,7 +135,7 @@ public class ClipRecyclerViewAdapter extends RecyclerView.Adapter<ClipRecyclerVi
         @Override
         @NonNull
         public String toString() {
-            return super.toString() ;
+            return super.toString();
         }
     }
 }
