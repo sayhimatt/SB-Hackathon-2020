d284dac1d63a1c7505f1a63e7392ad6f477d355e
Camposm <camposm1205@gmail.com>
2020-09-26 05:58:51 -0400

    Create ClipDialogBuilder.java

---
+++ QuickClips/app/src/main/java/com/fourstooges/quickclips/ClipDialogBuilder.java
@@ -0,0 +1,11 @@
+package com.fourstooges.quickclips;
+
+import android.app.AlertDialog;
+import android.content.Context;
+
+public class ClipDialogBuilder extends AlertDialog.Builder {
+    public ClipDialogBuilder(Context context) {
+        super(context);
+        
+    }
+}
