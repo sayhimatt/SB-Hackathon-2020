cd21fa1c8e01bf7aa21bdd8301e54292739786fd
Camposm <camposm1205@gmail.com>
2020-09-26 09:59:46 -0400

    Create ClipEditActivity.java

---
+++ QuickClips/app/src/main/java/com/fourstooges/quickclips/activity/ClipEditActivity.java
@@ -0,0 +1,16 @@
+package com.fourstooges.quickclips.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fourstooges.quickclips.R;
+
+public class ClipEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clip_edit);
+    }
+}
