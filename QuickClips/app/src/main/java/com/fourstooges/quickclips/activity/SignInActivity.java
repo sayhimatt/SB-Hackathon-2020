70709651cf0c4c2938964ea12b167a0979e3ccfa
Camposm <camposm1205@gmail.com>
2020-09-26 00:45:05 -0400

    Update SignInActivity.java

--- QuickClips/app/src/main/java/com/fourstooges/quickclips/activity/SignInActivity.java
+++ QuickClips/app/src/main/java/com/fourstooges/quickclips/activity/SignInActivity.java
@@ -38,24 +38,9 @@ public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        boolean matt = false;
-        if (matt) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            BottomNavigationView navView = findViewById(R.id.nav_view);
-            // Passing each menu ID as a set of Ids because each
-            // menu should be considered as top level destinations.
-            AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                    R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                    .build();
-            NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-            NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-            NavigationUI.setupWithNavController(navView, navController);
-        } else {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_signin);
-            createRequest();
-        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+        createRequest();
     }
 
     public void createRequest() {
