b9a0fa18a93b6e703f4c607cfa1c8e7e6585c74e
kiefuh <61067888+kiefuh@users.noreply.github.com>
2020-09-26 03:56:16 -0400

    Fixed a silly mistake

--- QuickClips/app/src/main/java/com/fourstooges/quickclips/ui/home/ClipRecyclerViewAdapter.java
+++ QuickClips/app/src/main/java/com/fourstooges/quickclips/ui/home/ClipRecyclerViewAdapter.java
@@ -73,7 +73,7 @@ public class ClipRecyclerViewAdapter extends RecyclerView.Adapter<ClipRecyclerVi
             }
         });
 
-        holder.copy_b.setOnClickListener(new View.OnClickListener() {
+        holder.modify_b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
