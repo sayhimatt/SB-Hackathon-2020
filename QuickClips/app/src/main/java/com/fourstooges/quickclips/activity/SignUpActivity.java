ee1fe6d70c8e08faed927259a7f62208360876e6
Camposm <camposm1205@gmail.com>
2020-09-26 02:51:40 -0400

    Update SignUpActivity.java

--- QuickClips/app/src/main/java/com/fourstooges/quickclips/activity/SignUpActivity.java
+++ QuickClips/app/src/main/java/com/fourstooges/quickclips/activity/SignUpActivity.java
@@ -1,15 +1,56 @@
 package com.fourstooges.quickclips.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.fourstooges.quickclips.MainActivity;
 import com.fourstooges.quickclips.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class SignUpActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        mAuth = FirebaseAuth.getInstance();
+        
+    }
+
+    public void signUp() {
+        String email = ((EditText) findViewById(R.id.tf_email)).getText().toString();
+        String password = ((EditText) findViewById(R.id.tf_password)).getText().toString();
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please Fill In The Empty Fields", Toast.LENGTH_LONG).show();
+        } else {
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(SignUpActivity.this, "Successfully Registered", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                        String currentUserID = mAuth.getCurrentUser().getUid();
+                        intent.putExtra("userID", currentUserID);
+                        setResult(RESULT_OK, intent);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        Toast.makeText(SignUpActivity.this, "Registration Failed", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
     }
 }
